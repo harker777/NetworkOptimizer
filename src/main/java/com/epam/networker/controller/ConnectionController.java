1bc84ea25593a4b536766fb52090750cabfe4824
harker777 <harker6666@gmail.com>
2013-11-06 10:49:48 +0200

    Views rearrangment

--- src/main/java/com/epam/networker/controller/ConnectionController.java
+++ src/main/java/com/epam/networker/controller/ConnectionController.java
@@ -10,21 +10,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  *
  * @author harker777
  */
-@RequestMapping("/connections")
 @Controller
 public class ConnectionController {
 
 	@Autowired
 	private ConnectionService connectionService;
 
-	@RequestMapping(method = RequestMethod.GET)
+	@RequestMapping(value = "/connections")
 	public String list(Model uiModel) {
-		uiModel.addAttribute("collections", connectionService.findAll());
+		uiModel.addAttribute("connections", connectionService.findAll());
 		return "connections/list";
 	}
 }
